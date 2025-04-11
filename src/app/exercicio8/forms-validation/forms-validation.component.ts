f4a76bec814abde27cffe966f71a3ba3b9cee76a
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-11 12:41:24 -0300

    commit inicial

---
+++ src/app/exercicio8/forms-validation/forms-validation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-validation',
+  imports: [],
+  templateUrl: './forms-validation.component.html',
+  styleUrl: './forms-validation.component.css'
+})
+export class FormsValidationComponent {
+
+  
+
+}
