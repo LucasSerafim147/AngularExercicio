e951c6a39e856544a2088ea611091ac8a16df5e4
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-11 13:25:07 -0300

    feat: adicionando validacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsValidationComponent } from "./exercicio8/forms-validation/forms-validation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsValidationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/exercicio8/forms-validation/forms-validation.component.ts
+++ src/app/exercicio8/forms-validation/forms-validation.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-validation',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms-validation.component.html',
   styleUrl: './forms-validation.component.css'
 })
 export class FormsValidationComponent {
 
-  
+nome = new FormControl('',[Validators.required, Validators.minLength(3)]);
+
+email =  new FormControl('',[Validators.required, Validators.email]);
+
+telefone = new FormControl('',[Validators.required,Validators.pattern(/^\d{10,11}$/)])
+
+
 
 }
